d25c24535ae8866752ac6e2fcfceddfc05236482
RamkumarSWE <115966371+RamkumarSWE@users.noreply.github.com>
2022-10-19 00:29:41 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,8 +1,34 @@
+/*
+ *******************************************************************************
+ * Structure Declaration
+ *******************************************************************************
+ */
+struct Stats {
+  float average;
+  float min;
+  float max;
+}s;
 
+/*
+ *******************************************************************************
+ * Function Declarations
+ *******************************************************************************
+ */
 struct Stats compute_statistics(const float* numberset, int setlength);
-
-typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
+/*
+ *******************************************************************************
+ * Type definitions
+ *******************************************************************************
+*/
+typedef void (*alerter_funcptr)();
+
+/*=============================================================================
+ =======                            VARIABLES                           =======
+ ==============================================================================
+ */
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
+
+/* EOF */
